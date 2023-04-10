78d96c0d4d9c1a744ee32927a501f327999a6434
Christian Zäske <blitzdose@gmail.com>
2023-04-11 01:25:33 +0200

    Added better padding to encryption

--- infinitrackStation/cryptor.py
+++ infinitrackStation/cryptor.py
@@ -8,14 +8,14 @@ import urandom
 class Cryptor:
 
     def __init__(self):
-        self.key = self.__read_key()
+        self.key = self._read_key()
 
         self.encryptor = cryptolib.aes(self.key, 1)
         self.decryptor = cryptolib.aes(self.key, 1)
 
     def set_key(self, key: bytes):
         self.key = key
-        self.__write_key(key)
+        self._write_key(key)
 
     def get_key(self):
         return self.key
@@ -34,33 +34,40 @@ class Cryptor:
         while q:
             ustruct.pack_into('>I', buf, 4 * (q - 1) + r, urandom.getrandbits(32))
             q -= 1
-        self.__write_key(buf)
+        self._write_key(buf)
         return buf
 
-    def encrypt(self, data: str):
-        data = self.__add_padding(data)
+    def encrypt(self, data: bytes):
+        data = self._add_padding(data)
         return self.encryptor.encrypt(data)
 
     def decrypt(self, data: bytes):
-        decrypted = self.decryptor.decrypt(data).replace(b"\x00", bytes()).decode('utf-8')
-        return decrypted
+        decrypted = self.decryptor.decrypt(data)
+        return self._remove_padding(decrypted)
 
-    def __add_padding(self, data):
-        data = data.encode('utf-8')
-        length = len(data)
+
+    def _add_padding(self, data: bytes):
+        length = len(data) + 1
         rest = (length % 16)
-        if rest > 0:
+        if rest == 0:
+            data = bytes(1) + data
+        else:
+            data = (16 - rest).to_bytes(1, 'big') + data
             data += bytes(16 - rest)
         return data
 
-    def __read_key(self):
+    def _remove_padding(self, data: bytes):
+        padding = int.from_bytes(data[:1], 'big')
+        return data[1:len(data)-padding]
+
+    def _read_key(self):
         try:
             f = open("lora.key")
             return binascii.unhexlify(f.readline())
         except OSError:
             return self.generate_key()
 
-    def __write_key(self, key):
+    def _write_key(self, key):
         f = open('lora.key', 'w')
         f.write(binascii.hexlify(key).decode('utf-8'))
         f.flush()
